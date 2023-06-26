4b2901d66365a742fde83b218d77f81ad3dc9114
AsepSupriyadi1 <asepsupyad789@gmail.com>
2023-06-26 14:33:17 +0700

    add backend

---
+++ mow_back-end/meals-on-wheels/src/test/java/com/summative/mow/mealsonwheels/MealsOnWheelsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.summative.mow.mealsonwheels;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MealsOnWheelsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

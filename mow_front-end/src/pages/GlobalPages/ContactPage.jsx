20b2877f9a0ac475a18412cecc4a73f735c16427
AsepSupriyadi1 <asepsupyad789@gmail.com>
2023-06-27 17:01:34 +0700

    syukur

--- mow_front-end/src/pages/GlobalPages/ContactPage.jsx
+++ mow_front-end/src/pages/GlobalPages/ContactPage.jsx
@@ -1,4 +1,3 @@
-
 import { Contactus } from "../../assets/images/Images";
 const ContactPage = () => {
   return (
@@ -15,35 +14,50 @@ const ContactPage = () => {
           </div>
         </div>
 
-			<div class="row">
-				
-				<div class="col-md-8 contact-page">
-          <h1>Location</h1>
-          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d262202.88671172387!2d107.29343528326255!3d-6.91518078792871!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e68e616cf485735%3A0x3d9935966c87148!2sDamn*21%20I%20Love%20Indonesia!5e0!3m2!1sid!2sid!4v1687859433414!5m2!1sid!2sid" width="700" height="450"  allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-					<div id="contact" class="contactForm clearfix">
-                        <div id="result"></div>
-                        <fieldset>
-                            <label for="name-id">First Name:<span>Put your name here</span></label>
-                            <input type="text" id="name-id" name="name-id"/>
-
-                        </fieldset>
-                        <fieldset> 
-                            <label for="surname-id">Last Name:<span>Put your surname here</span></label>   
-                            <input type="text" id="surname-id" name="surname-id"/>
-                        </fieldset>
-                        <fieldset>
-                            <label for="email-id">E-mail:<span>Type email address</span></label>
-                            <input type="text" id="email-id" name="email-id"/>
-                        </fieldset>
-                        <fieldset>
-                            <label for="message">Message:<span>Type email address</span></label>
-                            <textarea name="message" id="message" rows="6"></textarea>
-                        </fieldset>
-                        <fieldset>
-                            <button id="submit_btn" class="btn main-btn" type="submit" name="">Send Message</button>
-                        </fieldset>
-                    </div>
-				</div> 
+        <div class="row">
+          <div class="col-md-8 contact-page">
+            <h1>Location</h1>
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d262202.88671172387!2d107.29343528326255!3d-6.91518078792871!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e68e616cf485735%3A0x3d9935966c87148!2sDamn*21%20I%20Love%20Indonesia!5e0!3m2!1sid!2sid!4v1687859433414!5m2!1sid!2sid"
+              width="700"
+              height="450"
+              allowfullscreen=""
+              loading="lazy"
+              referrerpolicy="no-referrer-when-downgrade"
+            ></iframe>
+            <div id="contact" class="contactForm clearfix">
+              <div id="result"></div>
+              <fieldset>
+                <label for="name-id">
+                  First Name:<span>Put your name here</span>
+                </label>
+                <input type="text" id="name-id" name="name-id" />
+              </fieldset>
+              <fieldset>
+                <label for="surname-id">
+                  Last Name:<span>Put your surname here</span>
+                </label>
+                <input type="text" id="surname-id" name="surname-id" />
+              </fieldset>
+              <fieldset>
+                <label for="email-id">
+                  E-mail:<span>Type email address</span>
+                </label>
+                <input type="text" id="email-id" name="email-id" />
+              </fieldset>
+              <fieldset>
+                <label for="message">
+                  Message:<span>Type email address</span>
+                </label>
+                <textarea name="message" id="message" rows="6"></textarea>
+              </fieldset>
+              <fieldset>
+                <button id="submit_btn" class="btn main-btn" type="submit" name="">
+                  Send Message
+                </button>
+              </fieldset>
+            </div>
+          </div>
 
           <div class="col-md-4">
             <div class="box-content">

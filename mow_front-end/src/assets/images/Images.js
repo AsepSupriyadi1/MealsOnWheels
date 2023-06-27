4b4f2411c9ad90320a3b453922558f2e804d4130
aLAM12346 <syukursidiqnurala@gmail.com>
2023-06-27 16:26:38 +0700

    adding contactus

--- mow_front-end/src/assets/images/Images.js
+++ mow_front-end/src/assets/images/Images.js
@@ -3,6 +3,16 @@ import thumb1 from "./placehold/gallery/thumb(1).jpg";
 import thumb2 from "./placehold/gallery/thumb(2).jpg";
 import thumb3 from "./placehold/gallery/thumb(3).jpg";
 import thumb4 from "./placehold/gallery/thumb(4).jpg";
+import thumb5 from "./placehold/gallery/thumb(5).jpg";
+import thumb6 from "./placehold/gallery/thumb(6).jpg";
+import thumb7 from "./placehold/gallery/thumb(7).jpg";
+import thumb8 from "./placehold/gallery/thumb(8).jpg";
+import thumb9 from "./placehold/gallery/thumb(9).jpg";
+import thumb10 from "./placehold/gallery/thumb(10).jpg";
+import thumb11 from "./placehold/events/thumb(1).jpg";
+import thumb22 from "./placehold/events/thumb(2).jpg";
+import thumb33 from "./placehold/events/thumb(3).jpg";
+
 import testimonial from "./placehold/testimonials/giver.jpeg"
 
 export const resources = {
@@ -19,3 +29,8 @@ export const gallery = {
 export const Testimonial = {
   testimonial,
 };
+
+export const Contactus = {
+  thumb5,thumb6,thumb7,thumb8,thumb9,thumb10,thumb11,thumb22,thumb33,
+};
+

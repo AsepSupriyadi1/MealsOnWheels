c0b639ac52aa3174974cec8806fa285d88296535
arash3355 <rahmanshalehudin@gmail.com>
2023-06-30 16:38:55 +0700

    driver-designed

--- mow_front-end/src/pages/PrivatePages/Driver/DetailsDelivery.jsx
+++ mow_front-end/src/pages/PrivatePages/Driver/DetailsDelivery.jsx
@@ -2,8 +2,8 @@ import { food } from "../../../assets/images/Images";
 
 
 const DetailsDelivery = () => {
-    return (
-        <div className="container">
+	return (
+		<div className="container">
 			<div class="page-header">
 				<div class="row">
 					<div class="col-md-6 col-sm-6">
@@ -15,49 +15,45 @@ const DetailsDelivery = () => {
 				</div>
 			</div>
 
-            <div className="container" style={{ display: "flex", justifyContent: "center" }}>
-            <div class="col-md-8">
-					<div class="member-list">
-						<div class="member-thumb" style={{backgroundSize:"cover"}}>
-							<img src={food.food1} height={380} alt=""/>
+			<div className="container" style={{ display: "flex", justifyContent: "center"}}>
+				<div class="col-md-8">
+					<div class="box-content" style={{boxShadow:"5px 5px 4px 0px #709567cf"}}>
+						<div class="member-thumb" style={{ backgroundSize: "cover" }}>
+							<img src={food.food1} height={380} alt="" />
 						</div>
 						<div class="member-content">
 							<div class="pull-left">
-                                <h4 class="widget-title"><span>DETAILS DELIVERY</span></h4>
+								<h4 class="widget-title"><span>DETAILS DELIVERY</span></h4>
 							</div>
 							<div class="clearfix"></div>
-                            <table>
-									<tr>
-										<td>Member's Name</td>
-										<td>: Jhon Doe</td>
-									</tr>
-									<tr>
-										<td>Package Name</td>
-										<td>: Meals Package B</td>
-									</tr>
-									<tr>
-										<td>Kitchen Address </td>
-										<td>: Majalaya, New York City</td>
-									</tr>
-									<tr>
-										<td>Member Address</td>
-										<td>: Kp. Legok Midar 001/021, Ciparay, Bandung</td>
-									</tr>
-								</table>
-								<div>
-									<p>Status</p>
-                                	<a href="#" class="btn main-btn text-center" style={{width:"100%"}}>PICK UP</a>
-								</div>
-                                
+							<div className="col-md-4">
+								<p>Member's Name </p>
+								<p>Package Name </p>
+								<p>Kitchen Address</p>
+								<p>Member Address</p>
+							</div>
+							<div className="col-md-8">
+								<p>: John Doe</p>
+								<p>: Meals Package B</p>
+								<p>: Majalaya, New York City</p>
+								<p>: Kp. Legok Midar 001/021, Ciparay, Bandung</p>
+							</div>
+
+
+							<div>
+								<p>Status</p>
+								<a href="#" class="btn main-btn text-center" style={{ width: "100%" }}>PICK UP</a>
+							</div>
+
 
 						</div>
 					</div>
-                </div>
-            </div>      
-        </div>
+				</div>
+			</div>
+		</div>
 
-        
-    )
+
+	)
 }
 
 export default DetailsDelivery;

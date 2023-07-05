15a8224911bf7a305a244649b65ef6d6d0e1465f
aLAM12346 <syukursidiqnurala@gmail.com>
2023-07-05 14:12:03 +0700

    a

--- mow_front-end/src/pages/GlobalPages/AboutPage.jsx
+++ mow_front-end/src/pages/GlobalPages/AboutPage.jsx
@@ -114,7 +114,7 @@ const AboutPage = () => {
                     </div>
                     <div class="event-content">
                       <h5 class="event-title ">
-                        <a href="event-detail.html" className="text-success">Abdul Rahman Solehudin</a>
+                        <a href="event-detail.html" className="text-success">Abdul Rahman Shalehudin</a>
                       </h5>
                       <h6 class="event-subtitle text-secondary">Functional and UAT Analysis</h6>
                     </div>
@@ -125,9 +125,67 @@ const AboutPage = () => {
                     </div>
                     <div class="event-content">
                       <h5 class="event-title">
-                        <a href="event-detail.html" className="text-success">Ajri Muhamad Siddik</a>
+                        <a href="event-detail.html" className="text-success">Ajri Muhamad Sidik</a>
                       </h5>
                       <h6 class="event-subtitle text-secondary">Funcitonal and Data Analysis</h6>
+                    </div>
+                  </li>
+                  <li class="event-item">
+                    <div class="event-thumb">
+                      <img src={Contactus.thumb33} alt="" />
+                    </div>
+                    <div class="event-content">
+                      <h5 class="event-title">
+                        <a href="event-detail.html" className="text-success">Asep Supriyadi</a>
+                      </h5>
+                      <h6 class="event-subtitle text-secondary">Back End Security Analysis</h6>
+                    </div>
+                  </li>
+                  <li class="event-item">
+                    <div class="event-thumb">
+                      <img src={Contactus.thumb33} alt="" />
+                    </div>
+                    <div class="event-content">
+                      <h5 class="event-title">
+                        <a href="event-detail.html" className="text-success">Darren Farrel Andrian</a>
+                      </h5>
+                      <h6 class="event-subtitle text-secondary">Scenario Analysis and Planning </h6>
+                    </div>
+                  </li>
+                  <li class="event-item">
+                    <div class="event-thumb">
+                      <img src={Contactus.thumb33} alt="" />
+                    </div>
+                    <div class="event-content">
+                      <h5 class="event-title">
+                        <a href="event-detail.html"className="text-success">Syukur Sidiq Nur Alam</a>
+                      </h5>
+                      <h6 class="event-subtitle text-secondary">Front End Designer and       <div class="box-content shadow">
+              <h4 class="widget-title">
+                <span className="text-success">Our Team</span>
+              </h4>
+              <div class="events-sidebar">
+                <ul>
+                  <li class="event-item">
+                    <div class="event-thumb">
+                      <img src={Contactus.thumb11} alt="" />
+                    </div>
+                    <div class="event-content">
+                      <h5 class="event-title ">
+                        <a href="event-detail.html" className="text-success">Abdul Rahman Solehudin</a>
+                      </h5>
+                      <h6 class="event-subtitle text-secondary">Functional and UI Analysis</h6>
+                    </div>
+                  </li>
+                  <li class="event-item">
+                    <div class="event-thumb">
+                      <img src={Contactus.thumb22} alt="" />
+                    </div>
+                    <div class="event-content">
+                      <h5 class="event-title">
+                        <a href="event-detail.html" className="text-success">Ajri Muhamad Siddik</a>
+                      </h5>
+                      <h6 class="event-subtitle text-secondary">Fungcitonal and Data Analysis</h6>
                     </div>
                   </li>
                   <li class="event-item">
@@ -161,6 +219,12 @@ const AboutPage = () => {
                         <a href="event-detail.html"className="text-success">Syukur Sidiq Nur Alam</a>
                       </h5>
                       <h6 class="event-subtitle text-secondary">Front End Designer and Develper</h6>
+                    </div>
+                  </li>
+                </ul>
+              </div>
+            </div>
+      Develper</h6>
                     </div>
                   </li>
                 </ul>

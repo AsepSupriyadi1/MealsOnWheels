fcf048e7e76370817161016accacdb1211b2b902
aLAM12346 <syukursidiqnurala@gmail.com>
2023-07-06 22:09:37 +0700

    s

--- mow_front-end/src/components/UtilComponent/Header.jsx
+++ mow_front-end/src/components/UtilComponent/Header.jsx
@@ -20,135 +20,119 @@ function Header() {
   return (
     <>
       <header className="site-header">
-  <div className="top-header">
-    <div className="container">
-      <div className="inner-top">
-        <div className="row">
-          <div className="col-lg-10 col-md-6 col-sm-6 col-xs-12 header-info-left">
-            <ul>
-              <li>
-                <a href="skype:+34234235322?action">
-                  <i className="fa fa-phone"></i>+34 234 2353 22
-                </a>
-              </li>
-              <li>
-                <a href="mailto:youremail@info.com">
-                  <i className="fa fa-envelope-o"></i>youremail@info.com
-                </a>
-              </li>
-            </ul>
-          </div>
-          <div className="col-lg-2 col-md-6 col-sm-6 col-xs-12 header-info-right">
-            <ul className="d-flex justify-content-center justify-content-sm-end">
-              <li className="mx-2">
-                <a href="#" title="Envelope">
-                  <i className="fa fa-envelope"></i>
-                </a>
-              </li>
-              <li className="mx-2">
-                <a href="#" title="Facebook">
-                  <i className="fa fa-facebook"></i>
-                </a>
-              </li>
-              <li className="mx-2">
-                <a href="#" title="Twitter">
-                  <i className="fa fa-twitter"></i>
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-  <div className="container">
-  <div className="main-header">
-    <div className="row d-flex-between-center">
-      <div className="col-lg col-md-5 col-sm-5 col-xs-12 d-flex mx-auto mx-lg-none"> 
-        <a href="index.html">
-          <img src={resources.logo1} className="my-logo img-fluid" alt="" style={{ height: "80px", width: "auto" }} />
-        </a>
-      </div>
-      <div className="col-lg col-md main-header-right">
-        <div className="social-search text-end">
-          <div className="search-form d-none d-lg-block">
-            <form method="get" name="SearchForm">
-              <fieldset>
-                <input type="text" name="s" id="s" placeholder="Search Here..." />
-              </fieldset>
-            </form>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-</div>
-
-  
-  <div className="menu-wrapper">
-    <div className="container">
-      <div className="inner-menu">
-        <div className="row">
-          <div className="col-lg-9 main-menu">
-            <nav>
-              <ul className="sf-menu">
-                <li className="dropdown">
-                  <a href="/home" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
-                    Home <span className="caret"></span>
-                  </a>
-                </li>
-                <li className="dropdown">
-                  <a href="/about" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
-                    About <span className="caret"></span>
-                  </a>
-                </li>
-                <li className="dropdown">
-                  <a href="/contact" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
-                    Contact Us <span className="caret"></span>
-                  </a>
-                </li>
-                <li>
-                <div className="search-form d-md-none d-block">
-              <form method="get" name="SearchForm">
-                <fieldset>
-                  <input type="text" name="s" id="s" placeholder="Search Here..." />
-                </fieldset>
-              </form>
-            </div>
-                </li>
-              </ul>
-            </nav>
-          </div>
-          {currentUser.name ? (
-            currentUser.name && (
-              <>
-                <div className="col-lg button-holder">
-                  <span className="fs-5">{currentUser.name}</span> |
-                  <button onClick={handleLogout} className="btn btn-danger">
-                    Logout
-                  </button>
+        <div className="top-header d-none d-lg-block">
+          <div className="container">
+            <div className="inner-top">
+              <div className="row">
+                <div className="col-md-6 col-sm-6 col-xs-12 header-info-left">
+                  <ul>
+                    <li>
+                      <a href="skype:+34234235322?action">
+                        <i className="fa fa-phone"></i>+34 234 2353 22
+                      </a>
+                    </li>
+                    <li>
+                      <a href="mailto:youremail@info.com">
+                        <i className="fa fa-envelope-o"></i>youremail@info.com
+                      </a>
+                    </li>
+                  </ul>
                 </div>
-              </>
-            )
-          ) : (
-            <>
-              <div className="col-lg button-holder">
-                <a href="/register" className="btn main-btn" style={{ marginRight: "10px" }}>
-                  Register
-                </a>
-                <a href="/login" className="btn main-btn">
-                  Login
+                <div className="col-md-6 col-sm-6 col-xs-12 header-info-right visible-md visible-lg">
+                  <ul>
+                    <li>
+                      <a href="#" title="Facebook">
+                        <FontAwesomeIcon icon={faEnvelope} />
+                      </a>
+                    </li>
+                    <li>
+                      <a href="#" title="Facebook">
+                        <FontAwesomeIcon icon={faFacebook} />
+                      </a>
+                    </li>
+                    <li>
+                      <a href="#" title="Facebook">
+                        <FontAwesomeIcon icon={faTwitter} />
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="container">
+          <div className="main-header">
+            <div className="row d-flex-between-center">
+              <div className="col-md col-sm-5 ">
+                <a href="/home">
+                  <img src={resources.logo1} classNameName="my-logo" alt="" style={{ height: "80px" }} />
                 </a>
               </div>
-            </>
-          )}
+            </div>
+          </div>
+        </div>
+        <div id="navbar" className="menu-wrapper visible-md visible-lg">
+      <div className="container">
+        <div className="inner-menu">
+          <div className="row">
+            <Navbar expand="md" variant="light" className="px-5">
+              <Navbar.Toggle aria-controls="navbarCollaps" className="mb-4 bg-light mt-3"  />
+              <Navbar.Collapse id="navbarCollapse">
+                <div className="search-form d-sm-block d-lg-none">
+                  <form method="get" name="SearchForm" className="mb-2">
+                    <fieldset>
+                      <input type="text" name="s" id="s" placeholder="Search Here..." className="w-100" />
+                    </fieldset>
+                  </form>
+                </div>
+
+                <div className="col-md">
+                  <Nav className="navbar-nav">
+                    <Nav.Item>
+                      <Nav.Link className="fs-6 border-start-secondary" href="/home">Home</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                      <Nav.Link className="fs-6" href="/about">About</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                      <Nav.Link className="fs-6" href="/contact">Contact Us</Nav.Link>
+                    </Nav.Item>
+                  </Nav>
+                </div>
+
+                {currentUser.name ? (
+                  currentUser.name && (
+                    <>
+                      <div className="col-md-3 button-holder">
+                        <span className="fs-5">{currentUser.name}</span> |
+                        <Button onClick={handleLogout} variant="danger">
+                          Logout
+                        </Button>
+                      </div>
+                    </>
+                  )
+                ) : (
+                  <>
+                    <div className="col-md-3 me-auto text-lg-end text-start ">
+                      <a href="/register" className="btn main-btn" style={{ marginRight: "10px" }}>
+                        Register
+                      </a>
+                      <a href="/login" className="btn main-btn">
+                        Login
+                      </a>
+                    </div>
+                  </>
+                )}
+              </Navbar.Collapse>
+            </Navbar>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-</header>
 
 
+      </header>
     </>
   );
 }
